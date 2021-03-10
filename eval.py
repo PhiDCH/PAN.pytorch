2360abd4005ec308fb36bd086267dc5c5c03c0ed
PhiDCH <59147911+PhiDCH@users.noreply.github.com>
2021-03-10 15:08:03 +0700

    Add files via upload

--- eval.py
+++ eval.py
@@ -7,7 +7,15 @@ import torch
 import shutil
 import numpy as np
 from tqdm.auto import tqdm
-from predict import Pytorch_model
+
+from model import get_model
+from post_processing import decode
+import torch 
+from torchvision import transforms
+import os
+import cv2
+import time
+
 from utils import cal_recall_precison_f1, draw_bbox
 
 torch.backends.cudnn.benchmark = True
@@ -21,6 +29,64 @@ parse.add_argument('--gt_path', default='../icdar2015/test_gt', type=str, help='
 parse.add_argument('--result_folder', default='./result', type=str, help='folder path to result images')
 
 args = parse.parse_args()
+
+class Pytorch_model:
+    def __init__(self, model_path, gpu_id=None):
+        '''
+        初始化pytorch模型
+        :param model_path: 模型地址(可以是模型的参数或者参数和计算图一起保存的文件)
+        :param gpu_id: 在哪一块gpu上运行
+        '''
+        self.gpu_id = gpu_id
+
+        if self.gpu_id is not None and isinstance(self.gpu_id, int) and torch.cuda.is_available():
+            self.device = torch.device("cuda:%s" % self.gpu_id)
+        else:
+            self.device = torch.device("cpu")
+        print('device:', self.device)
+        checkpoint = torch.load(model_path, map_location=self.device)
+        
+        config = checkpoint['config']
+        config['arch']['args']['pretrained'] = False
+        self.net = get_model(config)
+
+        self.img_channel = config['data_loader']['args']['dataset']['img_channel']
+        self.net.load_state_dict(checkpoint['state_dict'])
+        self.net.to(self.device)
+        self.net.eval()
+
+    def predict(self, img: str, short_size: int = 736):
+        '''
+        对传入的图像进行预测，支持图像地址,opecv 读取图片，偏慢
+        :param img: 图像地址
+        :param is_numpy:
+        :return:
+        '''
+        assert os.path.exists(img), 'file is not exists'
+        img = cv2.imread(img)
+        if self.img_channel == 3:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        h, w = img.shape[:2]
+        scale = short_size / min(h, w)
+        img = cv2.resize(img, None, fx=scale, fy=scale)
+        # 将图片由(w,h)变为(1,img_channel,h,w)
+        tensor = transforms.ToTensor()(img)
+        tensor = tensor.unsqueeze_(0)
+
+        tensor = tensor.to(self.device)
+        with torch.no_grad():
+            if str(self.device).__contains__('cuda'):
+                torch.cuda.synchronize(self.device)
+            start = time.time()
+            preds = self.net(tensor)[0]
+            if str(self.device).__contains__('cuda'):
+                torch.cuda.synchronize(self.device)
+            preds, boxes_list = decode(preds)
+            scale = (preds.shape[1] / w, preds.shape[0] / h)
+            if len(boxes_list):
+                boxes_list = boxes_list / scale
+            t = time.time() - start
+        return preds, boxes_list, t
 
 def main(model_path, img_folder, save_path, gpu_id):
     if os.path.exists(save_path):
